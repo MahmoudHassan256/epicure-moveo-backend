04a3c1db6e98699ecf339846872326d93a5f68c6
MrHazrdY <103119019+MrHazrdY@users.noreply.github.com>
2022-09-23 18:41:18 +0300

    aa

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,9 @@ app.get("/", function (req, res) {
 app.use(routes);
 
 connectDb().then(async () => {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Our app is running on port ${ PORT }`);
-    });});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});});
 
 
