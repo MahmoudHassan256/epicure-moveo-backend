fb8c15b43faf7c7bbbd328d6834781e28b8779f9
MrHazrdY <103119019+MrHazrdY@users.noreply.github.com>
2022-09-23 18:15:30 +0300

    a

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import chefsRoutes from './chefs.routes';
 import restaurantsRoutes from './restaurants.routes';
-import dishesRoutes from './dishes.routes'
+import dishesRoutes from './dishes.routes';
+import usersRoutes from './users.routes';
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ const router = express.Router();
 router.use('/api/chefs/', chefsRoutes);
 router.use('/api/restaurants/', restaurantsRoutes);
 router.use('/api/dishes/', dishesRoutes);
-
+router.use('/api/users/',usersRoutes);
 export default router;
 
 
